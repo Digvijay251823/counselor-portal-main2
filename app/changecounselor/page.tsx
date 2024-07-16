6be31350634d1c6fa68d38e45449e798c193d323
Digvijay25182316 <digvijays.edake2002@gmail.com>
2024-07-16 16:21:25 +0530

    commit

--- app/changecounselor/page.tsx
+++ app/changecounselor/page.tsx
@@ -1,29 +1,11 @@
+"use client";
 import React from "react";
 import ErrorComponent from "@/Components/utils/ErrorPage";
 import data from "@/Counselors.json";
-import dynamic from "next/dynamic";
-const ChangeForm = dynamic(() => import("@/Components/counselee/ChangeForm"));
-
-// async function getCounselors() {
-//   unstable_noStore();
-//   const response = await fetch(`${SERVER_URL}/Counselor`);
-//   try {
-//     if (response.ok) {
-//       const responseData = await response.json();
-//       return responseData;
-//     } else {
-//       const errorData = await response.json();
-//       throw new Error(errorData);
-//     }
-//   } catch (error: any) {
-//     throw new Error(error);
-//   }
-// }
+import ChangeForm from "@/Components/counselee/ChangeForm";
 
 async function page() {
   try {
-    // const response = await getCounselors();
-    // if (response)
     return (
       <div className="w-full">
         <ChangeForm counselors={data} />
